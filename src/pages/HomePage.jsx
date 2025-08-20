72d1f6339319876326bbd11edb53fa414e31d261
MexNL <mex_kelters@hotmail.com>
2025-08-20 21:14:15 +0200

    Sectie 1 van de hoofdpagina toegevoegd, die is nu klaar

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,19 @@
+import "./HomePage.css"
 
 
 function HomePage(){
 
     return(
-        <p>Home Page</p>
+        <div>
+            <section className="section-one">
+                <img src="src/assets/bottles.png" alt="Bar with bottles"/>
+                <h2>Discover your next favorite cocktail</h2>
+                <hr/>
+            </section>
+            <section>
+
+            </section>
+        </div>
     )
 }
 
