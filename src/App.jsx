073de4e606352d73bee0829b279cee11748dd653
Mex <mex_kelters@hotmail.com>
2025-09-01 21:41:04 +0200

    Al wat authenticatie logica erin gefietst

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 
 import './App.css'
 import Navbar from "./components/Navbar/Navbar.jsx";
-import {Route, Routes} from "react-router-dom";
+import {Navigate, Route, Routes} from "react-router-dom";
 import HomePage from './pages/HomePage/HomePage.jsx'
 import SearchPage from './pages/SearchPage/SearchPage.jsx'
 import LoginPage from './pages/LoginPage/LoginPage.jsx'
 import RegisterPage from './pages/RegisterPage/RegisterPage.jsx'
 import ProfilePage from './pages/ProfilePage/ProfilePage.jsx'
 import ProductPage from './pages/ProductPage/ProductPage.jsx'
+import { AuthContext } from './context/AuthContext';
+import {useContext} from "react";
 
 function App() {
+    const { isAuth } = useContext(AuthContext);
   return (
     <>
         <Navbar/>
@@ -18,7 +21,7 @@ function App() {
             <Route path='/searchpage' element={<SearchPage/>}/>
             <Route path='/loginpage' element={<LoginPage/>}/>
             <Route path='/registerpage' element={<RegisterPage/>}/>
-            <Route path='/profilepage' element={<ProfilePage/>}/>
+            <Route path='/profilepage' element={isAuth ? <ProfilePage/> : <Navigate to="/" />} />
             <Route path='/product' element={<ProductPage/>}/>
         </Routes>
     </>
---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,51 @@
+import React, {createContext, useEffect, useState} from 'react';
+import {useNavigate} from 'react-router-dom';
+
+export const AuthContext = createContext({});
+
+function AuthContextProvider({children}) {
+    const [auth, setAuth] = useState({
+        isAuth: false,
+        user: null,
+        email: null,
+        status: "pending",
+    });
+    useEffect(() => {
+
+    }, []);
+    const navigate = useNavigate();
+
+    function login(userDetails) {
+        localStorage.setItem("token", userDetails.token);
+
+        setAuth({
+                isAuth: true,
+                user: userDetails.user.name,
+                email: userDetails.user.email,
+                status: "done",
+            },
+        );
+        navigate('/profile');
+
+    }
+
+    function logout() {
+        console.log('Gebruiker is uitgelogd!');
+
+        navigate('/');
+    }
+
+    const contextData = {
+        isAuth: auth.isAuth,
+        login: login,
+        logout: logout,
+    }
+
+    return (
+        <AuthContext.Provider value={contextData}>
+            {auth.status === 'done' ? children : <p>Loading...</p>}
+        </AuthContext.Provider>
+    );
+}
+
+export default AuthContextProvider;
