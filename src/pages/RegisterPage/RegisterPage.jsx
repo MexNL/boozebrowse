f2a51be5038c8382b1461766be93c1a147c0fe77
Mex <mex_kelters@hotmail.com>
2025-09-01 16:46:43 +0200

    Register pagina ook klaar zonder logica

--- src/pages/RegisterPage/RegisterPage.jsx
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -1,10 +1,61 @@
 
-
+import axios from "axios";
+import '../RegisterPage/RegisterPage.css'
+import { Link } from "react-router-dom";
 
 function RegisterPage(){
 
     return(
-        <p>Register page</p>
+        <div className="main-container">
+            <section className="register-container">
+                <header className="register-header">
+                    <h1>Register</h1>
+                </header>
+
+                <article className="register-container-body">
+                    <form>
+                        <label htmlFor="email">Email</label>
+                        <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            placeholder="Mail adress"
+                            className="register-input"
+                            required
+                        />
+
+                        <label htmlFor="password">Password</label>
+                        <input
+                            type="password"
+                            id="password"
+                            name="password"
+                            placeholder="Password"
+                            className="register-input"
+                            required
+                        />
+
+                        <label htmlFor="confirmPassword">Confirm Password</label>
+                        <input
+                            type="password"
+                            id="confirmPassword"
+                            name="confirmPassword"
+                            placeholder="Repeat Password"
+                            className="register-input"
+                            required
+                        />
+
+                        <button type="submit" className="register-button">Register</button>
+                    </form>
+
+                    <footer className="register-footer">
+                        <p>
+                            Already have an account?{" "}
+                            <Link to="../LoginPage">Login here</Link>
+                        </p>
+                    </footer>
+                </article>
+            </section>
+        </div>
     )
 }
 
